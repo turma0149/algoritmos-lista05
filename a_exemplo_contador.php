67499c95d8a9299e80a1356f4c103e2043a7257c
Michelle Belli <progweb0149@gmail.com>
2026-07-20 20:48:07 -0300

    Complemento

--- a_exemplo_contador.php
+++ a_exemplo_contador.php
@@ -1,32 +1,81 @@
 <?php
 
 echo ("<h1> Quiz do Chaves (V ou F) </h1>");
-
+ 
 echo ("<ol>");
-echo ("<li> O Chaves mora em um barril. </li>");
-echo ("<li> A Chiquinha é filha do Seu Madruga. </li>");
-echo ("<li> O Quico mora na casa da Bruxa do 71. </li>");
-echo ("<li> O Senhor Barriga é dono da vila. </li>");
-echo ("<li> O Professor Girafales trabalha na escola da vila. </li>");
-echo ("<li> O Chaves sempre come sanduiche de presunto. </li>");
-echo ("<li> A Dona Florinda é casada com o Seu Madruga. </li>");
-echo ("<li> O Nhonho é filho do Senhor Barriga. </li>");
-echo ("<li> O Chave usa um boné vermelho. </li>");
-echo ("<li> A Bruxa do 71 é apaixonada pelo Seu Madruga. </li>");
+echo ("<li> O Chaves mora em um barril. (V) </li>");
+echo ("<li> A Chiquinha é filha do Seu Madruga. (V) </li>");
+echo ("<li> O Quico mora na casa da Bruxa do 71. (F)</li>");
+echo ("<li> O Senhor Barriga é dono da vila. (V) </li>");
+echo ("<li> O Professor Girafales trabalha na escola da vila. (V) </li>");
+echo ("<li> O Chaves sempre come sanduiche de presunto. (F) </li>");
+echo ("<li> A Dona Florinda é casada com o Seu Madruga. (F)</li>");
+echo ("<li> O Nhonho é filho do Senhor Barriga. (V) </li>");
+echo ("<li> O Chaves usa um boné vermelho. (F)</li>");
+echo ("<li> A Bruxa do 71 é apaixonada pelo Seu Madruga. (V) </li>");
 echo ("</ol>");
 
 //Respostas do competidor (aleatórias com rand)
 
-$p1 = rand(0, 1);
-$p2 = rand(0, 1);
-$p3 = rand(0, 1);
-$p4 = rand(0, 1);
-$p5 = rand(0, 1);
-$p6 = rand(0, 1);
-$p7 = rand(0, 1);
-$p8 = rand(0, 1);
-$p9 = rand(0, 1);
-$p10 = rand(0, 1);
+$p1 = rand(0, 1); //V
+$p2 = rand(0, 1); //V
+$p3 = rand(0, 1); //F
+$p4 = rand(0, 1); //V
+$p5 = rand(0, 1); //V
+$p6 = rand(0, 1); //F
+$p7 = rand(0, 1); //F
+$p8 = rand(0, 1); //F
+$p9 = rand(0, 1); //V
+$p10 = rand(0, 1);//V
+
+$contador = 0;
+
+if($p1 == true){
+    $contador += 1;
+}
+
+if($p2 == true){
+    $contador += 1;
+}
+
+if($p3 == false){
+    $contador += 1;
+}
+
+if($p4 == true){
+    $contador += 1;
+}
+
+if($p5 == true){
+    $contador += 1;
+}
+
+if($p6 == false){
+    $contador += 1;
+}
+
+if($p7 == false){
+    $contador += 1;
+}
+
+if($p8 == true){
+    $contador += 1;
+}
+
+if($p9 == false){
+    $contador += 1;
+}
+
+if($p10 == false){
+    $contador += 1;
+}
+
+
+
+
+
+
+
 
 echo ("<h1> Respostas do competidor </h1>");
 
@@ -40,3 +89,7 @@ echo ("P7:" . $p7 . "<br>");
 echo ("P8:" . $p8 . "<br>");
 echo ("P9:" . $p9 . "<br>");
 echo ("P10:" . $p10 . "<br>");
+
+
+
+?>
